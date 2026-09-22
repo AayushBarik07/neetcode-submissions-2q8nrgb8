ef46ed0abc83c92b28e866159bdb0e70b078baf1
Aayush Barik <117500786+AayushBarik07@users.noreply.github.com>
2026-09-22 23:38:24 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        map<int, int> mpp;
+
+        for(int i=0; i<n; i++) {
+            if(mpp.find(target - nums[i]) != mpp.end()) {
+                return {mpp[target - nums[i]], i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
